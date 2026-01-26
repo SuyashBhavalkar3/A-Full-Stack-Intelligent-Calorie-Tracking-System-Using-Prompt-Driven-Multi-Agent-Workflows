6d93266e738cc3fbdc448d9ba4341cdd86e02cbe
Suyash Bhavalkar <suyashbhavalkar82@gmail.com>
2026-01-26 11:50:26 +0530

    Changes in databse file to retrieve internal postgreSQL on render

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "sqlite:///./test.db"  # Change to PostgreSQL URL as needed
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
